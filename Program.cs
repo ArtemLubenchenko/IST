a64975ef3cf9a21c1db45c5cb72c109207c90190
Artem <lubenchenko.artem@gmail.com>
2018-03-27 17:17:01 +0300

    Лабораторная работа №1

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, z1, z2;
+            Console.WriteLine("a=");
+            a = Convert.ToDouble(Console.ReadLine());
+            z1 = (Math.Cos(a) + Math.Sin(a)) / (Math.Cos(a) - Math.Sin(a));
+            Console.WriteLine(z1);
+            z2 = (Math.Tan(2 * a) + 1 / Math.Cos(2 * a));
+            Console.WriteLine(z2);
+            Console.ReadLine();
+        }
+    }
+}
